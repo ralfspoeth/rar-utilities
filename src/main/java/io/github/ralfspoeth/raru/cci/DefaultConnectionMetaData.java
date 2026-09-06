ebe5c8c0b63526fb6cc8e2df56ea865f6c6c80bd
ralfspoeth <ralf.spoeth@gmail.com>
2026-09-07 00:33:23 +0200

    a record instead of a class for ConnectionMetaData

--- src/main/java/io/github/ralfspoeth/raru/cci/DefaultConnectionMetaData.java
+++ src/main/java/io/github/ralfspoeth/raru/cci/DefaultConnectionMetaData.java
@@ -2,30 +2,12 @@ package io.github.ralfspoeth.raru.cci;
 
 import jakarta.resource.cci.ConnectionMetaData;
 
-import java.util.Objects;
-
 /**
- * This class provides a canonical
- * implementation of the interface
- * {@link ConnectionMetaData}; with three final fields for the
- * required meta data properties.
- *
- * @author Ralf Spöth
- * @version 1.0
+ * A record that implements {@link ConnectionMetaData}.
  */
-public class DefaultConnectionMetaData implements ConnectionMetaData {
-
-    private final String
-            eisProductName,
-            eisProductVersion,
-            userName;
-
-    public DefaultConnectionMetaData(String eisProductName, String eisProductVersion, String userName) {
-        this.eisProductName = eisProductName;
-        this.eisProductVersion = eisProductVersion;
-        this.userName = userName;
-    }
-
+public record DefaultConnectionMetaData(String eisProductName, String eisProductVersion, String userName)
+        implements ConnectionMetaData
+{
     @Override
     public String getEISProductName() {
         return eisProductName;
@@ -39,20 +21,5 @@ public class DefaultConnectionMetaData implements ConnectionMetaData {
     @Override
     public String getUserName() {
         return userName;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof DefaultConnectionMetaData md) {
-            return
-                    Objects.equals(eisProductName, md.eisProductName) &&
-                            Objects.equals(eisProductVersion, md.eisProductVersion) &&
-                            Objects.equals(userName, md.userName);
-        } else return false;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(eisProductName, eisProductVersion, userName);
     }
 }
